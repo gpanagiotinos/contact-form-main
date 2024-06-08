478032344375e5cc0621ba8f4f5659404b4a4546
Giorgos Panagiotinos <g.panagiotinos@swiftmarine.eu>
2024-06-08 20:57:33 +0300

    need to check for accessibility of the form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,7 @@
-function onClickQueryType(element, inputId, unCheckedEl) {
-  const input = document.querySelector(`#${inputId}`);
-  unCheckedEl.classList.remove("label-checked");
-  if (input) {
-    element.classList.add("label-checked");
-    input.checked = true;
-  }
-}
 function main() {
-  const generalEnquiryEl = document.querySelector("#label-generalEnquiry");
-  const supportRequestEl = document.querySelector("#label-supportRequest");
-  const formEl = document.querySelector("#form-contact-us");
-  generalEnquiryEl.onclick = onClickQueryType.bind(
-    this.onClickQueryType,
-    generalEnquiryEl,
-    "generalEnquiry",
-    supportRequestEl
-  );
-  supportRequestEl.onclick = onClickQueryType.bind(
-    this.onClickQueryType,
-    supportRequestEl,
-    "supportRequest",
-    generalEnquiryEl
-  );
+  const email = document.forms["contact-us"]["emailAddress"];
+  email.oninput = (evt) => {
+    this.hasEmailError(evt.target.value);
+  };
 }
 main();
--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,17 +1,55 @@
-function hasError(id, name) {
-  const element = document.getElementById(id);
-  console.log(element);
-  element.classList.remove("none");
+function hasErrorText(id, value) {
+  const element = document.getElementById(`${id}-error`);
+  if (value === null || value === "") {
+    element.classList.remove("none");
+  } else {
+    element.classList.add("none");
+  }
+  return;
+}
+function hasErrorCheckbox(id, value) {
+  const element = document.getElementById(`${id}-error`);
+  if (!value) {
+    element.classList.remove("none");
+  } else {
+    element.classList.add("none");
+  }
+  return;
+}
+function hasEmailError(value) {
+  const emailPattern = /[^@\s]+@[^@\s]+/;
+  const requiredError = document.getElementById(`email-required-error`);
+  const invalidError = document.getElementById(`email-invalid-error`);
+  if (!emailPattern.test(value)) {
+    invalidError.classList.remove("none");
+  } else {
+    invalidError.classList.add("none");
+  }
+  if (value === null || value === "") {
+    requiredError.classList.remove("none");
+  } else {
+    requiredError.classList.add("none");
+  }
+  return;
 }
 
 function submitForm() {
+  const toast = document.getElementById("toast")
+  toast.classList.add("success");
+  return false;
+}
+function validateForm() {
   const firstName = document.forms["contact-us"]["First Name"].value;
   const lastName = document.forms["contact-us"]["Last Name"].value;
+  const email = document.forms["contact-us"]["emailAddress"].value;
   const queryType = document.forms["contact-us"]["query_type"].value;
   const message = document.forms["contact-us"]["message"].value;
-  const consentType = document.forms["contact-us"]["consent_type"].value;
-  if (firstName === null || firstName === "") {
-    showError("firstName");
-  }
-  return false;
+  const consentType = document.forms["contact-us"]["consent_type"].checked;
+  hasErrorText("firstName", firstName);
+  hasErrorText("lastName", lastName);
+  hasErrorText("message", message);
+  hasErrorText("queryType", queryType);
+  hasEmailError(email);
+  hasErrorCheckbox("consent", consentType);
+  return;
 }
